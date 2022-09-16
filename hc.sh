945a021671ec0e73008590cc293b3114f7ca518b
Mike Lindner <mikelindner@mail.com>
2022-09-16 15:21:52 +1000

    Seems to work!

--- hc.sh
+++ hc.sh
@@ -1,4 +1,4 @@
 # Hard Copy - Quickly send text files to printer
 
-enscript -1rG --color=1 --line-numbers -p /tmp/out.ps --highlight=bash -c $0
+enscript -1rG --color=1 --line-numbers -p /tmp/out.ps --highlight=bash -c $1
 lp -o sides=one-sided /tmp/out.ps
